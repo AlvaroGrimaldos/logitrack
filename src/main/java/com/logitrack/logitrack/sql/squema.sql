37609546979330027b24617920ec4d5b6148ed52
AlvaroGrimaldos <alvarogrimaldos21@gmail.com>
2025-11-11 09:56:48 -0500

    feat: :tada: Start the project

---
+++ src/main/java/com/logitrack/logitrack/sql/squema.sql
@@ -0,0 +1,75 @@
+use logitrack_db;
+
+create table usuario (
+	id bigint auto_increment primary key,
+	nombre varchar(100) not null,
+	email varchar(200) not null unique,
+	password_hash varchar(300) not null,
+	rol enum('admin', 'empleado') not null,
+	activo boolean default true,
+	created_at datetime default CURRENT_TIMESTAMP()
+);
+
+create table bodega (
+	id bigint auto_increment primary key,
+	nombre varchar(100) not null,
+	ubicacion varchar(255) not null,
+	capacidad int not null,
+	encargado_id bigint not null,
+	activo boolean default true,
+	created_at datetime default CURRENT_TIMESTAMP(),
+	updated_at datetime default CURRENT_TIMESTAMP() on update current_timestamp(),
+	foreign key(encargado_id) references usuario(id)
+);
+
+create table producto (
+	id bigint auto_increment primary key,
+	nombre varchar(100) not null,
+	categoria varchar(50) not null,
+	precio decimal(10,2) not null,
+	activo boolean default true,
+	created_at datetime default CURRENT_TIMESTAMP(),
+	update_at datetime default CURRENT_TIMESTAMP() on update current_timestamp()
+);
+
+create table inventario (
+	id bigint auto_increment primary key,
+	producto_id bigint not null,
+	bodega_id bigint not null,
+	stock_actual int not null,
+	foreign key(producto_id) references producto(id),
+	foreign key(bodega_id) references bodega(id)
+);
+
+create table movimiento_inventario (
+	id bigint auto_increment primary key,
+	fecha datetime not null,
+	tipo enum('entrada', 'salida', 'transferencia') not null,
+	usuario_id bigint not null,
+	bodega_origen_id bigint,
+	bodega_destino_id bigint,
+	created_at datetime default CURRENT_TIMESTAMP(),
+	foreign key (bodega_origen_id) references bodega(id),
+	foreign key (bodega_destino_id) references bodega(id)
+);
+
+create table movimiento_detalle (
+	id bigint auto_increment primary key,
+	movimiento_id bigint not null,
+	producto_id bigint not null,
+	cantidad int not null,
+	foreign key(movimiento_id) references movimiento_inventario(id),
+	foreign key(producto_id) references producto(id)
+);
+
+create table auditoria_cambios (
+	id bigint auto_increment primary key,
+	fecha_hora datetime not null,
+	usuario_id bigint not null,
+	tipo_operacion enum('insert', 'update', 'delete') not null,
+	entidad_afectada varchar(100) not null,
+	id_entidad_afectada bigint not null,
+	valores_antes JSON,
+	valores_despues JSON,
+	foreign key(usuario_id) references usuario(id)
+);
