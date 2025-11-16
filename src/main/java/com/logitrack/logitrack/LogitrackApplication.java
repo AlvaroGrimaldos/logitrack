18f662ab6c5e67f3c0dcb3dd47ae0977587335ba
AlvaroGrimaldos <alvarogrimaldos21@gmail.com>
2025-11-16 16:26:21 -0500

    feat: :zap: Se configura para su despliegue con apache tomcat y se finaliza el proyecto

--- src/main/java/com/logitrack/logitrack/LogitrackApplication.java
+++ src/main/java/com/logitrack/logitrack/LogitrackApplication.java
@@ -2,12 +2,20 @@ package com.logitrack.logitrack;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class LogitrackApplication {
+public class LogitrackApplication extends SpringBootServletInitializer{
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(LogitrackApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(LogitrackApplication.class, args);
 	}
+
 
 }
